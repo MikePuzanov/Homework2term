67d6964948b407632812a1859d8ca102dfce4f1e
MikePuzanov <mikepuzanov@mail.ru>
2021-03-05 23:38:15 +0300

    add ReverseBWT and Test

--- hw1BWT/hw1BWT/BWT.cs
+++ hw1BWT/hw1BWT/BWT.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
-using System.Text;
 
 namespace hw1BWT
 {
     class BWT
     {
-        static void SwapStrings(char[,] transpositionTable, int i, int j)
+        public static bool Test()
+        {
+            string string1 = "abcab";
+            var (result, numberOfString) = BWTtransform(string1);
+            if (result != "cbaab")
+            {
+                return false;
+            }
+            string oldString1 = ReverseBWT("cbaab", numberOfString);
+            return (oldString1 == string1);
+        }
+        private static void SwapStrings(char[,] transpositionTable, int i, int j)
         {
             int size = transpositionTable.GetLength(0);
             for (int t = 0; t < size; ++t)
@@ -34,7 +43,7 @@ namespace hw1BWT
             return false;
         }
 
-        static void SortTable(char[,] table)
+        private static void SortTable(char[,] table)
         {
             int size = table.GetLength(0);
             for (int i = 0; i < size; ++i)
@@ -49,7 +58,7 @@ namespace hw1BWT
             }
         }
 
-        public static Tuple<string, int> BWTransform(string str)
+        public static Tuple<string, int> BWTtransform(string str)
         {
             int size = str.Length;
             char[,] table = new char[size, size];
@@ -96,6 +105,67 @@ namespace hw1BWT
             }
             var result = new String(resultString);
             return new Tuple<string, int>(result, numberOfStr);
+        }
+
+        private static char[] GetAlphabet(char[] str)
+        {
+            string alphabet = "";
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (!alphabet.Contains(str[i]))
+                {
+                    alphabet += str[i];
+                }
+            }
+            char[] alphaberArray = alphabet.ToCharArray(0, alphabet.Length);
+            Array.Sort(alphaberArray);
+            return alphaberArray;
+        }
+
+        public static string ReverseBWT(string str, int numberOfStr)
+        {
+            char[] line = str.ToCharArray(0, str.Length);
+            char[] alphabetOfString = GetAlphabet(line);
+            var countSymbolsInStr = new int[alphabetOfString.Length];
+            for (int i = 0; i < alphabetOfString.Length; ++i)
+            {
+                countSymbolsInStr[i] = line.Where(x => x == alphabetOfString[i]).Count();
+            }
+            var numerationSymbols = new int[alphabetOfString.Length];
+            int sumSymbols = 0;
+            for (int i = 0; i < countSymbolsInStr.Length; ++i)
+            {
+                sumSymbols += countSymbolsInStr[i];
+                if (i == 0)
+                {
+                    numerationSymbols[i] = 0;
+                }
+                else
+                {
+                    numerationSymbols[i] = sumSymbols - countSymbolsInStr[i];
+                }
+            }
+            var helper = new int[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+                for (int j = 0; j < alphabetOfString.Length; j++)
+                {
+                    if (line[i] == alphabetOfString[j])
+                    {
+                        helper[i] = numerationSymbols[j];
+                        numerationSymbols[j]++;
+                    }
+                }
+            }
+            var answer = new char[line.Length];
+            for (int i = line.Length - 1; i > -1; i--)
+            {
+                answer[i] = line[numberOfStr];
+                numberOfStr = helper[numberOfStr];
+            }
+            var result = new String(answer);
+            result = result.Substring(0, result.Length);
+            return result;
         }
     }
 }
--- hw1BWT/hw1BWT/Program.cs
+++ hw1BWT/hw1BWT/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace hw1BWT
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            if (!BWT.Test())
+            {
+                Console.WriteLine("Тест не пройден!");
+                return;
+            }
+            Console.WriteLine("Тест пройден!");
+            Console.WriteLine("Введите строку: ");
+            string line = Console.ReadLine();
+            var (result, numberOfString) = BWT.BWTtransform(line);
+            Console.WriteLine("Строка после алгоритма: " + result);
+            string res2 = BWT.ReverseBWT(result, numberOfString);
+            Console.WriteLine("Исходная строка: " + res2);
         }
     }
 }
