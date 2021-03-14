e72b626e8745b7e10f4ceb21a1fcfd973847d46d
MikePuzanov <mikepuzanov@mail.ru>
2021-03-14 21:54:00 +0300

    only create a project

---
+++ hw2LZW/hw2LZW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw2LZW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
