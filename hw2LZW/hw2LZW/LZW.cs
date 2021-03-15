2c330e04b46d84fd26b05b3d81539071ad498239
MikePuzanov <mikepuzanov@mail.ru>
2021-03-16 00:44:22 +0300

    finish trie and start LZW

---
+++ hw2LZW/hw2LZW/LZW.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace hw2LZW
+{
+    class LZW
+    {
+        public static void Compress(string file)
+        {
+            using var readFile = new FileStream(file, FileMode.Open);
+            file += ".zipped";
+            using var writeFile = new FileStream(file, FileMode.CreateNew);
+            var root = new Trie();
+            var byter = readFile.ReadByte();
+            for (int i = 0; i < readFile.Length; ++i)
+            {
+
+                if (i == readFile.Length - 1)
+                {
+
+                }
+            }
+        }
+
+        public static void Decompress()
+        {
+
+        }
+    }
+}
---
+++ hw2LZW/hw2LZW/Trie.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw2LZW
+{
+    public class Trie
+    {
+        private class Node
+        {
+            public byte IdSymbol { get; set; }
+
+            public int IndexSymbol { get; set; }
+
+            public int CodeBytes { get; set; }
+
+            public Dictionary<byte, Node> Sons { get; set; }
+
+            public Node(byte idSymbol, int indexSymbol)
+            {
+                IdSymbol = idSymbol;
+                IndexSymbol = indexSymbol;
+                Sons = new Dictionary<byte, Node>();
+            }
+
+            public Node IsFind(byte value)
+            => Sons.TryGetValue(value, out Node node) ? node : null;
+        }
+
+        private Node root = new Node(0, 0);
+
+        private Node runner;
+
+        public Trie()
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                InitRoot((byte)i, i);
+            }
+        }
+
+        public int CountCodes { get; set; }
+
+        public int LastCode { get; set; }
+
+        private void InitRoot(byte idSymbol, int index)
+        {
+            var son = new Node(idSymbol, index);
+            root.Sons.Add(idSymbol, son);
+            CountCodes++;
+        }
+
+        private void Init(byte value)
+        {
+            var son = new Node(value, CountCodes);
+            runner.Sons.Add(value, son);
+            son.CodeBytes = CountCodes;
+            CountCodes++;
+        }
+
+        private bool CheckAdd(byte value, ref Node node)
+            => node.IsFind(value) == null ? true : false;
+
+        public int IsAdd(byte value)
+        {
+            var isCheck = CheckAdd(value, ref runner);
+            if (isCheck)
+            {
+                var son = new Node(value, CountCodes);
+                son.CodeBytes = CountCodes;
+                CountCodes++;
+                runner.Sons.Add(value, son);
+                runner = root;
+                return LastCode;
+            }
+            else
+            {
+                runner = runner.Sons[value];
+                LastCode = runner.CodeBytes;
+                return -1;
+            }    
+        }
+    }
+}
