7b000321caa5d3f2e86bed5265cfc655f4f0bf0e
MikePuzanov <mikepuzanov@mail.ru>
2021-04-11 02:46:14 +0300

    add createGraph and not finished AlgorithmPrima

---
+++ hw5Routers/hw5Routers/AlgorithmPrima.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw5Routers
+{
+    class AlgorithmPrima
+    {
+        static int maxKey(int[] key, bool[] mstSet)
+        {
+            int max = int.MinValue;
+            int indexMax = -1;
+            for (int i = 0; i < key.Length; i++)
+                if (mstSet[i] == false && key[i] > max)
+                {
+                    max = key[i];
+                    indexMax = i;
+                }
+            return indexMax;
+        }
+
+        static void Algorithm(int[,] matrix)
+        {
+            int size = matrix.Length;
+            int[] parent = new int[size]; 
+            int[] key = new int[size];
+            bool[] isConsist = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                key[i] = int.MaxValue;
+                isConsist[i] = false;
+            }
+            key[0] = 0;
+            parent[0] = -1;
+            for (int count = 0; count < size - 1; count++)
+            {
+                int u = maxKey(key, isConsist);
+                isConsist[u] = true;
+                for (int v = 0; v < size; v++)
+                {
+                    if (matrix[u, v] != 0 && isConsist[v] == false && matrix[u, v] < key[v])
+                    {
+                        parent[v] = u;
+                        key[v] = matrix[u, v];
+                    }
+                }
+            }
+            ///
+            ///
+            ///
+        }
+    }
+}
---
+++ hw5Routers/hw5Routers/Routers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace hw5Routers
+{
+    public class Routers
+    {
+        public int[,] CreateGraph(string filePath)
+        {
+            int vertices = CountsVertices(filePath);
+            var matrix = new int[vertices, vertices];
+            var file = new StreamReader(filePath);
+            string stringLine = file.ReadLine();
+            while (stringLine != null)
+            {
+                stringLine = stringLine.Replace(':', ' ');
+                stringLine = stringLine.Replace(',', ' ');
+                string[] lineDrop = stringLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var numberFirst = Int32.Parse(lineDrop[0]);
+                for (int i = 1; i < lineDrop.Length; ++i)
+                {
+                    var numberSecond = Int32.Parse(lineDrop[i]);
+                    var distance = Int32.Parse(lineDrop[i + 1].Substring(1, lineDrop.Length - 2));
+                    matrix[numberFirst, numberSecond] = matrix[numberSecond, numberFirst] = distance;
+                }
+            }
+            return matrix;
+        }
+
+        public int CountsVertices(string path)
+        {
+            var file = new StreamReader(path);
+            string stringLine = file.ReadLine();
+            var list = new List<int>();
+            int vertice = 0;
+            while (stringLine != null)
+            {
+                stringLine = stringLine.Replace(':', ' ');
+                stringLine = stringLine.Replace(',', ' ');
+                string[] lineDrop = stringLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var number = Int32.Parse(lineDrop[0]);
+                if (!list.Contains(number))
+                {
+                    list.Add(number);
+                    vertice++;
+                }
+                for (int i = 0; i < lineDrop.Length / 2; ++i)
+                {
+                    number = Int32.Parse(lineDrop[2 * i + 1]);
+                    if (!list.Contains(number))
+                    {
+                        list.Add(number);
+                        vertice++;
+                    }
+                }
+            }
+            return vertice;
+        }
+
+        public void WriteInFile(int[,] matrix, string filePath)
+        {
+            FileInfo fileOut = new FileInfo(filePath);
+            if (fileOut.Exists)
+            {
+                fileOut.Delete();
+            }
+            FileStream currentFile = new FileStream(filePath, FileMode.Create);
+            StreamWriter writer = new StreamWriter(currentFile);
+
+
+        }
+    }
+}
