35db16a754903bf8c6ef335fcce40840ff828f74
MikePuzanov <mikepuzanov@mail.ru>
2021-04-16 12:26:17 +0300

    Queue

---
+++ test2/test2/QueueException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    public class QueueException : Exception
+    {
+        public QueueException()
+        {
+        }
+
+        public QueueException(string message)
+        : base(message)
+        {
+        }
+    }
+}
