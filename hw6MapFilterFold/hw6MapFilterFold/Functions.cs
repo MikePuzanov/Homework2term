0e423f5d010feeecf537fcd14d1335743408a03a
MikePuzanov <89803250877@mail.ru>
2021-04-17 02:56:47 +0300

    add function and test

---
+++ hw6MapFilterFold/hw6MapFilterFold/Functions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6MapFilterFold
+{
+    public static class Functions<T>
+    {
+        public static List<T> Map(List<T> list, Func<T, T> function)
+        {
+            int count = list.Count;
+            var resultList = new List<T>();
+            foreach (var node in list)
+            {
+                resultList.Add(function(node));
+            }
+            return resultList;
+        }
+
+        public static List<T> Filter(List<T> list, Func<T, bool> function)
+        {
+            var returnList = new List<T>();
+            foreach (var node in list)
+            {
+                if (function(node))
+                {
+                    returnList.Add(node);
+                }
+            }
+            return returnList;
+        }
+
+        public static T Fold(List<T> list, T startValue,Func<T, T, T> function)
+        {
+            foreach (var node in list)
+            {
+                startValue = function(startValue, node);
+            }
+            return startValue;
+        }
+    }
+}
---
+++ hw6MapFilterFold/hw6MapFilterFold/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hw6MapFilterFold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listInt = new List<int>() { 1, 2, 3 };
+            var resultList1 = new List<int>() { 2, 4, 6 };
+            var list1 = Functions<int>.Map(listInt, x => x * 2);
+
+            var listString = new List<string>() { "a", "b", "c" };
+            var resultList2 = new List<string>() { "aaaa", "baaa", "caaa" };
+            var list2 = Functions<string>.Map(listString, x => x + "aaa");
+
+            var listChar = new List<char>() { 'a', 'b', 'c' };
+            var resultList3 = new List<char>() { 'd', 'd', 'd' };
+            var list3 = Functions<char>.Map(listChar, x => 'd');
+
+        }
+    }
+}
