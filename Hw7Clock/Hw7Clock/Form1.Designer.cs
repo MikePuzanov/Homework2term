f4e6c26cc8094ce5b2aa889f299b6a9f4d261b1a
MikePuzanov <89803250877@mail.ru>
2021-05-12 22:51:20 +0300

    start code

---
+++ Hw7Clock/Hw7Clock/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Hw7Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundBox = new System.Windows.Forms.PictureBox();
+            this.hourBox = new System.Windows.Forms.PictureBox();
+            this.minuteBox = new System.Windows.Forms.PictureBox();
+            this.secondBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundBox
+            // 
+            this.backgroundBox.Location = new System.Drawing.Point(54, 32);
+            this.backgroundBox.Name = "backgroundBox";
+            this.backgroundBox.Size = new System.Drawing.Size(350, 350);
+            this.backgroundBox.TabIndex = 0;
+            this.backgroundBox.TabStop = false;
+            // 
+            // hourBox
+            // 
+            this.hourBox.Location = new System.Drawing.Point(74, 52);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(350, 350);
+            this.hourBox.TabIndex = 1;
+            this.hourBox.TabStop = false;
+            // 
+            // minuteBox
+            // 
+            this.minuteBox.Location = new System.Drawing.Point(111, 69);
+            this.minuteBox.Name = "minuteBox";
+            this.minuteBox.Size = new System.Drawing.Size(350, 350);
+            this.minuteBox.TabIndex = 2;
+            this.minuteBox.TabStop = false;
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 450);
+            this.Controls.Add(this.secondBox);
+            this.Controls.Add(this.minuteBox);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.backgroundBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backgroundBox;
+        private System.Windows.Forms.PictureBox hourBox;
+        private System.Windows.Forms.PictureBox minuteBox;
+        private System.Windows.Forms.PictureBox secondBox;
+    }
+}
+
---
+++ Hw7Clock/Hw7Clock/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hw7Clock
+{
+    public partial class Form1 : Form
+    {
+        Bitmap back, hour, minute, second;
+
+        public Form1()
+        {
+            InitializeComponent();
+            back = new Bitmap("..\\background.png");
+            hour = new Bitmap("..\\hour.png");
+            minute = new Bitmap("..\\minute.png");
+            second = new Bitmap("..\\second.png");
+        }
+
+        private Bitmap Rotate(Bitmap rotater, float angle)
+        {
+
+        }
+    }
+}
