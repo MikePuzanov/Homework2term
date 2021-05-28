e37550105ddcbc3c240ddd1ea84029c8f9eb5ab8
MikePuzanov <89803250877@mail.ru>
2021-05-28 12:47:39 +0300

    Test with BubleSort

---
+++ Test2.1/Test2.1/CompareStringInDescendingOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2._1
+{
+    /// <summary>
+    /// сравнивает string, обратное обычное Compare
+    /// </summary>
+    public class CompareStringInDescendingOrder : Comparer<string>
+    {
+        public override int Compare(string value1, string value2)
+        {
+            return value2.CompareTo(value1);
+        }
+    }
+}
---
+++ Test2.1/Test2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>() { "abc", "aa", "bb", "zv", "fe", "qwe", "123" };
+            BubbleSort.Sort(list, new CompareStringInDescendingOrder());
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
